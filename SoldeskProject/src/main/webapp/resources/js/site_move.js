54b25f761af09d2f90c2fc4bf8bb5af6f036ae93
Onneoul <kimminse99@naver.com>
2019-12-06 09:57:06 +0900

    view에 info 추가

---
+++ SoldeskProject/src/main/webapp/resources/js/site_move.js
@@ -0,0 +1,23 @@
+function toTheaterInfo() {
+	location.href = "theaterInfo.com/theater";
+}
+
+function toTheaterDetail(theater_number) {
+	location.href = "theaterInfo.com/theater/Detail?theater_number=" + theater_number;
+}
+
+function toPersonInfo(person_number) {
+	location.href = "theaterInfo.com/person?person_number=" + person_number;
+}
+
+function toGenreInfo(genre_number) {
+	location.href = "theaterInfo.com/genre?genre_number" + genre_number;
+}
+
+function toStageInfo(stage_number) {
+	location.href = "theaterInfo.com/stage?stage_number" + stage_number;
+}
+
+function toGroupInfo(group_number) {
+	location.href = "theaterInfo.com/group?group_number" + group_number;
+}
